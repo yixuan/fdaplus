f63093d1904f7fc527379cd4f5fa4fc854b7094e
Yixuan Qiu <yixuan.qiu@cos.name>
2014-09-23 22:43:50 -0400

    refine definition of fd+ class

--- R/fd.R
+++ R/fd.R
@@ -1,6 +1,8 @@
-setClassUnion("vecmat", c("numeric", "matrix"))
-
-setClass("fd+", slots = c(coefs = "vecmat",
-                          basis = "basis+")
+setClass("fd+", slots = c(coefs = "matrix", basis = "basis+"),
+         validity = function(object) {
+             if(ncol(object$coefs) != object@basis@ncoef)
+                 return("ncol(coefs) must be equal to the number of basis functions")
+             return(TRUE)
+         }
 )
 
