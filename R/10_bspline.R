4f801060b31994d51ef8cdce2f48bec7814d8454
Yixuan Qiu <yixuan.qiu@cos.name>
2015-05-09 19:36:46 -0400

    documentation for basis_bs()

--- R/10_bspline.R
+++ R/10_bspline.R
@@ -1,10 +1,47 @@
+#' Creating B-spline Basis Functions
+#'
+#' This function constructs a \code{\link[=bspline+-class]{bspline+}} object
+#' that represents a series of B-spline basis functions.
+#'
+#' \code{nbasis}, \code{order} and \code{breaks} are related to each other,
+#' satisfying the condition \code{nbasis == order + length(breaks)}. Hence when
+#' any one of \code{nbasis} and \code{breaks} is properly specified,
+#' the other one can be computed accordingly. For details,
+#'
+#' \itemize{
+#'   \item If both \code{nbasis} and \code{breaks} are \code{NULL} (the default),
+#'         \code{nbasis} will be set to \code{order} and there is no inner knot.
+#'   \item If \code{nbasis} is \code{NULL} and \code{breaks} is given, then
+#'         \code{nbasis} will be calculated using the equation above.
+#'   \item If \code{nbasis} is given and \code{breaks} unspecified, a series
+#'         of equally spaced inner knots will be calculated.
+#'   \item If both \code{nbasis} and \code{breaks} are provided, then the program
+#'         will check their validity.
+#' }
+#'
+#' @param range A length-two numeric vector to define the interval on which
+#'              basis functions can be evaluated. Default is \code{c(0, 1)}.
+#' @param nbasis The total number of basis functions, including the ones that need
+#'               to be dropped. See section \strong{Details}.
+#' @param order The order of the B-spline functions, which is the degree of
+#'              splines plus one. Default is 4, standing for cubic splines.
+#' @param breaks A vector of break points (inner knots) to define the B-spline basis.
+#'               This does not include the end points, which are defined by
+#'               the parameter \code{range}. See section \strong{Details}.
+#' @param dropind Indices of basis functions that need to be dropped. Default is
+#'                \code{NULL}, meaning no basis will be dropped.
+#'
+#' @return A \code{\link[=bspline+-class]{bspline+}} object representing the
+#'         basis functions.
+#' @author Yixuan Qiu <\url{http://statr.me/}>
+#' @export
 basis_bs = function(range = c(0, 1), nbasis = NULL, order = 4,
                     breaks = NULL, dropind = NULL)
 {
     range = as.numeric(range)
     order = as.integer(order)
     dropind = unique(as.integer(dropind))
-    
+
     if(is.null(nbasis) & is.null(breaks))
     {
         nbasis = order
@@ -26,7 +63,7 @@ basis_bs = function(range = c(0, 1), nbasis = NULL, order = 4,
         if(nbasis != order + length(knots))
             stop("nbasis must be equal to order plus the number of inner knots")
     }
-    
+
     new("bspline+",
         range = range,
         nbasis = nbasis,
@@ -66,7 +103,7 @@ setMethod("%*%", signature(x = "bspline+", y = "bspline+"),
           function(x, y) {
               if(!isTRUE(all.equal(x@range, y@range)))
                   stop("range of x and y must be the same")
-              
+
               ordx = as.integer(x@degree + 1)
               ordy = as.integer(y@degree + 1)
               allknotsx = as.numeric(c(rep(x@range[1], ordx), x@knots,
